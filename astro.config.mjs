6d13199b773142502ab27556230da5c77e2028b0
JuanZuniga7 <jczp_020401@hotmail.com>
2024-09-07 22:48:12 +0000

    minor changes I just need to commit that

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,11 @@ import tailwind from '@astrojs/tailwind';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-        integrations: [react(), tailwind(), sitemap()]
+  integrations: [react(), tailwind(), sitemap()],
+  output: 'server',
+  adapter: vercel()
 });
---
+++ src/modules/home/data/asideNav.ts
@@ -0,0 +1,9 @@
+import type ILink from "../../../core/data/interfaces/ILink";
+
+const asideNav: ILink[] = [
+  { label: "Top", path: "" },
+  { label: "Resume", path: "#resume" },
+  { label: "This project", path: "#this-project" },
+]
+
+export default asideNav;
