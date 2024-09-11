8accbde433004267208617cc661d0fd3c34b25b4
JuanZuniga7 <jczp_020401@hotmail.com>
2024-09-10 22:34:26 -0500

    aside fixes

--- src/core/data/card.ts
+++ src/core/data/card.ts
@@ -11,37 +11,40 @@ import mail from "../assets/icons/mail.astro"
 import linkedin from "../assets/icons/linkedin.astro"
 import instagram from "../assets/icons/instagram.astro"
 import twitter from "../assets/icons/twitter.astro"
+import resume from "../assets/icons/resume.astro"
 
-interface ICard{
-    profile: IAsset;
-    nav: ILinkWithIcon[];
-    social: ILinkWithIcon[];
+interface ICard {
+        profile: IAsset;
+        nav: ILinkWithIcon[];
+        social: ILinkWithIcon[];
 }
 
-const getCard = () : ICard => {
-    return en;
+const getCard = (): ICard => {
+        return en;
 }
 
-const en:ICard = {
-    profile: {
-        path: profile,
-        alt: "Profile Picture",
-    },
-    nav: [
-        {label: "Home",path: "/", icon: home},
-        {label: "About",path: "/about", icon: info},
-        {label: "Blog",path: "#soon", icon: blog},
-        {label: "Projects",path: "/projects", icon: projects},
-        {label: "Friends",path: "/friends", icon: friends},
-        {label: "Contact",path: "/contact", icon: mail},
-    ],
-    social: [
-        {label: "Github",path: "", icon: projects},
-        {label: "LinkedIn",path: "", icon: linkedin},
-        {label: "Instagram",path: "", icon: instagram},
-        {label: "Twitter",path: "", icon: twitter},
+const en: ICard = {
+        profile: {
+                path: profile,
+                alt: "Profile Picture",
+        },
+        nav: [
+                { label: "Home", path: "/", icon: home },
+                { label: "About", path: "/about", icon: info },
+                { label: "Resume", path: "/resume", icon: resume },
+                { label: "Blog", path: "#soon", icon: blog },
+                { label: "Projects", path: "/projects", icon: projects },
+                { label: "Friends", path: "/friends", icon: friends },
+                { label: "Contact", path: "/contact", icon: mail },
+        ],
+        social: [
+                { label: "Github", path: "https://github.com/JuanZuniga7", icon: projects },
+                { label: "Github", path: "https://github.com/RustedCor", icon: projects },
+                { label: "LinkedIn", path: "https://www.linkedin.com/in/zunigajuan7", icon: linkedin },
+                { label: "Instagram", path: "https://www.instagram.com/rusted.cor79", icon: instagram },
+                { label: "Twitter", path: "https://x.com/rusted_cor", icon: twitter },
 
-    ]
+        ]
 }
 
 export default getCard;
--- src/modules/home/data/asideNav.ts
+++ src/modules/home/data/asideNav.ts
@@ -1,9 +1,8 @@
 import type ILink from "../../../core/data/interfaces/ILink";
 
 const asideNav: ILink[] = [
-  { label: "Top", path: "" },
-  { label: "Resume", path: "#resume" },
-  { label: "This project", path: "#this-project" },
+        { label: "Resume", path: "#resume" },
+        { label: "This project", path: "#this-project" },
 ]
 
 export default asideNav;
